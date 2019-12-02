9598596e5c0ac27e777b6be5f63d1279ce593006
Angela <8798027+angelabauer@users.noreply.github.com>
2019-12-02 10:54:37 +0530

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,40 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
-      ),
+          home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.indigoAccent,
+        ),
+        body: MagicBall(),
+      )),
     );
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int number = 1;
+  void changeBallFace() {
+    setState(() {
+      number = Random().nextInt(5) + 1;
+      print('I got clicked $number');
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+          onPressed: changeBallFace,
+          child: Image.asset('images/ball$number.png')),
+    );
+  }
+
+
+}
